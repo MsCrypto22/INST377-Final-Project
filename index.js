955dde2ecfe5becc9f7115493ac6609f443ef3ff
miamanning <mmanning@terpmail.umd.edu>
2024-12-15 23:48:41 -0500

    more cors troubleshooting

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const supabaseClient = require('@supabase/supabase-js');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
