abb076b141cb771b51fe669894b77c63bd1ab5bc
miamanning <mmanning@terpmail.umd.edu>
2024-12-15 23:33:53 -0500

    cors troubleshooting 2

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
